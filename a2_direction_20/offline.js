def4f4f716431b3332ddf79e0fe2564513901a7b
smart40702 <s40702@bj.ac.th>
2024-12-04 13:39:03 +0700

    Add files via upload

---
+++ a2_direction_20/offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1733294331,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/collide-sheet0.png",
+		"images/playeranimation-sheet0.png",
+		"images/playeranimation-sheet1.png",
+		"images/player-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
