465b588738efeb8ee6e12f500047e57a7da29e5e
smart40702 <s40702@bj.ac.th>
2025-02-05 14:55:43 +0700

    Add files via upload

---
+++ a4_slicer_20/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1738741941,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/botcut-sheet0.png",
+		"images/spawn-sheet0.png",
+		"images/fruitslash-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
