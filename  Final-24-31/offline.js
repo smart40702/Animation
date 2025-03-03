abf1dfabfbe43354164b096c73eabbf3ab0263d2
smart40702 <s40702@bj.ac.th>
2025-03-04 00:13:29 +0700

    Add files via upload

---
+++  Final-24-31/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1741021862,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/player-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/player2-sheet0.png",
+		"images/player2-sheet1.png",
+		"images/coinbox-sheet0.png",
+		"images/coin-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite7-sheet0.png",
+		"images/enemie-sheet0.png",
+		"images/enemie-sheet1.png",
+		"images/enemie-sheet2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
